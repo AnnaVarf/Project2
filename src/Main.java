7801694e47aa94e56912e2c3daa63ad40014e338
Anna Var <juuni24@gmail.com>
2023-07-13 15:08:51 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        int initialBalance = 1100;
-        int addend = 900;
+        int price = 13_676;
 
-        int bonus = 0;
-        if (addend > 1000) {
-            bonus = addend / 100;
-        }
+        int bonus = price / 20;
 
-            int finalBalance = initialBalance + addend + bonus;
-            System.out.println("Итоговый счет:" + finalBalance);
-            System.out.println("Бонус:" + bonus);
+        System.out.println("Бонус:" + bonus);
 
-        }
     }
+}
